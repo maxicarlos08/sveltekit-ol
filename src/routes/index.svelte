fd7cec52fe7952803516ef8c7de8503056cde5f2
Maxicarlos08 <maxicarlos08@gmail.com>
2021-12-13 19:18:57 +0100

    added reproduction stuff

---
+++ src/routes/index.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+	import { browser } from '$app/env';
+	import { Map, View } from 'ol';
+	import { OSM } from 'ol/source';
+	import { Tile } from 'ol/layer';
+    import "ol/ol.css"
+	import { onMount } from 'svelte';
+
+	let target: HTMLDivElement;
+	let map: Map;
+
+	onMount(() => {
+		if (browser) {
+			const view = new View({ center: [0, 0], zoom: 2 });
+			const layers = [new Tile({ source: new OSM() })];
+			map = new Map({ target, view, layers });
+		}
+	});
+</script>
+
+<div class="ol-container" bind:this={target} />
+
+<style>
+	:global(html, body, #svelte),
+	div {
+		width: 100%;
+		height: 100%;
+        margin: 0;
+        padding: 0;
+	}
+</style>
